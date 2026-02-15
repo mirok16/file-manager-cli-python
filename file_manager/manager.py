6390a3a7eda4e9a575e179a461b955dfba5e80da
mirok16 <154271539+mirok16@users.noreply.github.com>
2026-02-15 14:17:04 +0300

    Add file deletion function

--- file_manager/manager.py
+++ file_manager/manager.py
@@ -4,3 +4,6 @@ def list_files(path="."):
 def create_file(filename):
     with open(filename, "w") as f:
         f.write("")
+def delete_file(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
